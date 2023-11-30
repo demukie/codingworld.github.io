c030c1a0acb4b6dd25b97ed0259e68dbc69c8e9e
houstonfb <147531017+houstonfb@users.noreply.github.com>
2023-11-30 10:47:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 alert('NO GALATASARAY FANS ALLOWED!!!')
 
-// Function to change the background color of the body
-function changeBackgroundColor() {
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.backgroundColor = randomColor;
-}
-
-// Event listener for a button click to change the background color
-document.getElementById('changeColorButton').addEventListener('click', changeBackgroundColor);
